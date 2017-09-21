1db9fc08ccb400589eb021cd4c71acf9ccc4160f
Chao Liu <lc.roy86@gmail.com>
2017-09-21 18:28:12 -0500

    in progress

--- applications/overset_application/custom_utilities/OversetAssembler.h
+++ applications/overset_application/custom_utilities/OversetAssembler.h
@@ -87,7 +87,7 @@ public:
 
                     if( a_node_id < b_node_id )
                         return true;
-                    else
+                    else if( a_node_id > b_node_id )
                         return false;
 
                     it_a = std::next(it_a);
@@ -114,8 +114,6 @@ public:
 
         const ModelPart::ElementsContainerType & r_elements_pointer = const_cast<ModelPart &>(mrModelPart).Elements();
         
-        std::size_t num_face = 0;
-
         for( ModelPart::ElementsContainerType::ptr_const_iterator it_p_element = r_elements_pointer.ptr_begin(); it_p_element != r_elements_pointer.ptr_end(); it_p_element = std::next(it_p_element) )
         {
             const Element::GeometryType::GeometriesArrayType faces = (* it_p_element)->GetGeometry().Faces();
@@ -135,15 +133,9 @@ public:
                     face_to_element_map[nodes_id] = {(* it_p_element),i};
                 else
                     face_to_element_map.erase(it);
-
-                num_face++;
-
-                DistributedAssignment::DataUtility::DataPrinter printer;
-                printer.Print(nodes_id);
             }
         }
 
-        std::cout<<__func__<<": num_face: "<<num_face<<std::endl;
         std::cout<<__func__<<": size face_to_element_map: "<<face_to_element_map.size()<<std::endl;
         
 
